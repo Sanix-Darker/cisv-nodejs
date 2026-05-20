7d140d6bf23361eb0f2fc3cb49eb72fea6cd2a82
sanix-darker <s4nixd@gmail.com>
2026-05-20 07:09:36 +0200

    Prepare Node.js 0.5.0 release

--- cisv/cisv/cisv_addon.cc
+++ cisv/cisv/cisv_addon.cc
@@ -2225,7 +2225,7 @@ Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
     CisvParser::Init(env, exports);
 
     // Add version info
-    exports.Set("version", Napi::String::New(env, "0.4.9"));
+    exports.Set("version", Napi::String::New(env, "0.5.0"));
 
     // Add transform type constants
     Napi::Object transformTypes = Napi::Object::New(env);
